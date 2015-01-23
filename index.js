c97334f3868c1ab8c338a11738e4b4efb5f35ade
Dakuan <dom.barker808@gmail.com>
2015-01-23 10:59:23 +0000

    more chaos

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
+var R = require('ramda');
+
 function _delay(time) {
+
+    // console.log(time);
     var time = time || 2000;
+
+    console.log('delaying by ' + time);
     return function(req, res, next) {
         setTimeout(function() {
             next();
@@ -7,8 +13,68 @@ function _delay(time) {
     }
 }
 
-module.exports = function(opts) {
+function _error(code) {
+    var code = code || 500;
+    console.log('throwing ' + code);
     return function(req, res, next) {
-    	_delay(opts.time).apply(null, arguments);
+        res.status(code);
+        res.end();
     }
-};
+}
+
+function _funnyInvoke(fn, o) {
+    return function(args) {
+        return fn.call(fn, parseArgs(o.call(o, args)));
+    }
+}
+
+function _buildAction(prop) {
+    return;
+}
+
+function _randomElement(array) {
+    return array[Math.floor(Math.random() * array.length)];
+}
+
+function _truthy(val) {
+    return !!val;
+}
+
+var isNumber = R.is(Number);
+
+var parseArgs = R.ifElse(isNumber, R.I, R.always(null));
+
+var chaos = R.ifElse(
+    _truthy,
+    R.cond(
+        [R.has('delay'), _funnyInvoke(_delay, R.prop('delay'))], [R.has('error'), _funnyInvoke(_error, R.prop('error'))]
+    ),
+    _randomElement([_delay, _error])
+);
+
+module.exports = chaos;
+
+// use
+
+// // anything can happen
+// root.use(chaos());
+
+// // delays by 2000 ms (default)
+// root.use(chaos({
+//     time: true
+// }));
+
+// // delays by custom time
+// root.use(chaos({
+//     time: 4000
+// }));
+
+// // throws 500 error (default)
+// root.use(chaos({
+//     error: true
+// }));
+
+// // throws custom error code
+// root.use(chaos({
+//     error: 401
+// }));
